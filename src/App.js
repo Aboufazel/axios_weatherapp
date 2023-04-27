fd562fb4e39e145316c4618b2342b1e86a835a33
aboufazel <a.ab2521@gmail.com>
2023-04-28 00:27:38 +0330

    feat : background image and index max head feat done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-
-import './App.css';
 import {handleGetOneDayWeather} from "./components/api";
-import {useEffect, useState} from "react";
-import Night from "./assets/pics/dark-clouds-during-rainy-weather.jpg"
-import Sun from "./assets/pics/sunset-background-featuring-sky-clouds.jpg"
+import {useEffect} from "react";
+import "./index.css"
+import CityCard from "./components/CityCard/CityCard";
+
 
 
 function App() {
-   const [day , setDay] = useState(false)
+
   const ShirazDetail = async ()=>{
     const data = await  handleGetOneDayWeather("shiraz");
     console.log(data)
   }
-
-
-  const date = new Date();
-
-   useEffect(()=>{
-     if(date.getHours() >= 12){
-       setDay(true)
-     }
-   } , [date])
-
 
 
   useEffect(()=>{
@@ -29,8 +18,8 @@ function App() {
   } , [])
 
   return (
-    <div style={{height:"100vh"  ,padding:"20px"}}>
-      jhdfjsdkjfhkfjghf
+    <div style={{height:"94.3vh"  ,padding:"20px"}}>
+       <CityCard/>
     </div>
   );
 }
