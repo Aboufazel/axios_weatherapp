82b4f0eebedfcdb6eb9172081f0b5caed1b4b479
aboufazel <a.ab2521@gmail.com>
2023-04-27 22:56:32 +0330

    feat : tailwind add in project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
 import {handleGetOneDayWeather} from "./components/api";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import Night from "./assets/pics/dark-clouds-during-rainy-weather.jpg"
+import Sun from "./assets/pics/sunset-background-featuring-sky-clouds.jpg"
+
 
 function App() {
-
+   const [day , setDay] = useState(false)
   const ShirazDetail = async ()=>{
     const data = await  handleGetOneDayWeather("shiraz");
     console.log(data)
   }
+
+
+  const date = new Date();
+
+   useEffect(()=>{
+     if(date.getHours() >= 12){
+       setDay(true)
+     }
+   } , [date])
+
 
 
   useEffect(()=>{
@@ -16,7 +29,8 @@ function App() {
   } , [])
 
   return (
-    <div className="App">
+    <div style={{height:"100vh"  ,padding:"20px"}}>
+      jhdfjsdkjfhkfjghf
     </div>
   );
 }
