0d2e413d76b2a12f0629819fdf43dfe2e9c0512f
aboufazel <a.ab2521@gmail.com>
2023-04-30 21:58:48 +0330

    feat : city search bar add and style done

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ function App() {
 
   return (
     <div className={"container flex flex-col items-center lg:w-max-[1800px] w-full"} style={{padding:"20px"}}>
+      <div className={"flex items-center w-full mb-4 searchInput h-[36px] p-[8px]"}>
+
+        <button>
+          <svg opacity={'50%'} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white"
+               className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+          </svg>
+        </button>
+
+
+        <input type={"search"} placeholder={"Search for a city or airport"} className={"w-full ml-[4px]"}/>
+      </div>
         {
             cityData.map(item => (
                 <div className={"container max-w-[500px] mx-auto flex flex-col items-center"} key={`city-card-show-first${item.id}`}>
