2085ed34484d954bc51daad97a4510ea49b333a3
aboufazel <a.ab2521@gmail.com>
2023-04-30 21:27:27 +0330

    ref : city card refactor done

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className={"container flex flex-col items-center lg:w-max-[1800px] w-full"} style={{padding:"20px"}}>
         {
             cityData.map(item => (
-                <div key={`city-card-show-first${item.id}`}>
+                <div className={"container max-w-[500px] mx-auto flex flex-col items-center"} key={`city-card-show-first${item.id}`}>
                     <CityCard name={item.name}/>
                 </div>
             ))
--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -1,18 +1,33 @@
 import "./cityCars.style.scss";
-import City from "../../assets/pics/washington.png";
-
+import Status from "../../assets/pics/weather-status/Sun cloud angled rain.png"
 const CityCard = ({name}) => {
 
 
 
     return(
-        <div className={"flex flex-col my-[15px] items-center gap-[5px] justify-center shadow-xl w-[280px] h-[180px] cityCard"}>
-            <div className={"flex flex-col items-center gap-3 justify-center rounded-[50%] w-[60px] h-[60px]"}>
-                <img src={City} alt={"weather-status"}/>
-                <h1 className={"font-extrabold text-xl"}>
-                    {name}
-                </h1>
-            </div>
+        <div className={"flex flex-col relative my-[35px] items-center py-[5px] px-[20px] justify-start shadow-xl w-[280px] h-[135px] cityCard"}>
+            <img src={Status} alt={"weather-status"} className={"absolute top-[-47px] right-[6px] w-[45%]"}/>
+                <div className={"flex w-full items-center"}>
+                   <h1 className={"font-semibolds text-[45px]"}>
+                    19°
+                   </h1>
+                </div>
+                <div className={"flex items-center justify-between w-full"}>
+                    <div className={"flex flex-col w-1/2"}>
+                        <p className={"flex items-center opacity-50 text-[14px] gap-[8px]"}>
+                            <span>H:16°</span>
+                            <span>L:8°</span>
+                        </p>
+                        <p className={"mt-[-3px] font-semibold"}>
+                            Fars, Shiraz
+                        </p>
+                    </div>
+                    <div className={"flex justify-end items-center w-1/2"}>
+                        <p className={"font-bold"}>
+                            {"Cloudy"}
+                        </p>
+                    </div>
+                </div>
         </div>
     )
 }
